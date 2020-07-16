e7bf9aa076df88bb3d50d475c20fa2d2554d7357
DEVELOPER ONE <dev1@124apps.com>
2020-07-16 15:03:36 +0530

    0.1: secure Store aded

---
+++ src/data/GlobalKeys.js
@@ -0,0 +1,5 @@
+export const SECRET_KEY = "prashanth";
+
+export const TEST_STR = "stringDataTest";
+export const TEST_JSON = "jsonDataTest";
+export const TEST_ARRAY = "arrayDataTest";
---
+++ src/data/Store.js
@@ -0,0 +1,26 @@
+import CryptoJS from "crypto-js";
+import { SECRET_KEY } from "./GlobalKeys";
+const DB = localStorage;
+export const Store = {
+   storeObjectData: function(key, data) {        
+        let val = CryptoJS.AES.encrypt(JSON.stringify(data), SECRET_KEY);
+        DB.setItem(key, val)        
+   },
+    storeStringData: function(key, data) {
+        let val = CryptoJS.AES.encrypt(data, SECRET_KEY);
+        DB.setItem(key, val)   
+        // let Decript = CryptoJS.AES.decrypt(val.toString(), secureKey);
+        // console.log("Decript data: ", Decript.toString(CryptoJS.enc.Utf8))
+    },
+
+    getObjectData: function (key){
+        let val = DB.getItem(key)
+        let Decript = CryptoJS.AES.decrypt(val.toString(), SECRET_KEY);
+        return JSON.parse(Decript.toString(CryptoJS.enc.Utf8));
+    },
+    getStringData: function (key){
+        let val = DB.getItem(key)
+        let Decript = CryptoJS.AES.decrypt(val.toString(), SECRET_KEY);
+        return Decript.toString(CryptoJS.enc.Utf8);
+    },
+}
